68a1e43b74862209cac0b4012ae64945104d4dba
nojipiz <nojipiz@protonmail.com>
2021-12-21 15:43:27 +0000

    Refactor manager methods

--- src/test/java/com/sergio/RegisterApp/AppTest.java
+++ src/test/java/com/sergio/RegisterApp/AppTest.java
@@ -3,8 +3,11 @@ package com.sergio.RegisterApp;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+
 import com.sergio.RegisterApp.model.Customer;
 import com.sergio.RegisterApp.model.DocType;
+import com.sergio.RegisterApp.model.RegisterManager;
 import com.sergio.RegisterApp.persistence.FileController;
 
 import org.junit.Test;
@@ -16,8 +19,7 @@ public class AppTest {
   /**
    * Rigorous Test :-)
    */
-  @Test
-  public void shouldAnswerWithTrue() {
+  public void fileControllerTest() {
     FileController fileController = new FileController();
     Customer customer = new Customer("David", "Rodriguez", DocType.citizenshipCard, "100235482", "08/12/2002");
     try {
@@ -27,5 +29,24 @@ public class AppTest {
     } catch (Exception e) {
       assertTrue(false);
     }
+  }
+
+  @Test
+  public void RegisterCustomerTest() {
+    Customer customer = new Customer("David", "Rodriguez", DocType.citizenshipCard, "100235482", "08/12/2002");
+    RegisterManager manager = new RegisterManager();
+    try {
+      manager.addCustomer(customer);
+      manager.getListCustomers().stream().forEach(x -> System.out.println(x.getFirstNames()));
+    } catch (Exception e) {
+      System.err.println(e);
+    }
+  }
+
+  @Test
+  public void IsValidID() {
+    RegisterManager manager = new RegisterManager();
+    boolean res = manager.isValidID("123");
+    assertTrue(res);
   }
 }
